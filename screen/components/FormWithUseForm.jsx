98a0e3d4ce584ce49e44fb898cb7f80244a92e11
Paula Cano <paulacanoroman@gmail.com>
2023-12-19 21:37:27 +0100

    feat: inputs and text

--- screen/components/FormWithUseForm.jsx
+++ screen/components/FormWithUseForm.jsx
@@ -7,9 +7,18 @@ const FormWithUseForm = () => {
 
   return (
     <>
-    <h2>Formulario con useForm</h2>
     <form>
-
+      <h2>Introduzca:</h2>
+      <h3>Nombre y apellidos</h3>
+      <input type='text'></input>
+      <h3>Contraseña</h3>
+      <input type='password'></input>
+      <h3>Edad</h3>
+      <input type='number'></input>
+      <h3>Email</h3>
+      <input type='mail'></input>
+      
+      <input type='submit' value='Enviar' />
     </form>
     </>
   )
