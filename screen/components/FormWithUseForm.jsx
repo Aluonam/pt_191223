c078bd47f39ba499aae93503476dcfb789a8ed7d
Paula Cano <paulacanoroman@gmail.com>
2023-12-19 21:41:00 +0100

    feat: show array by console
    use useForm property; {...register('name')}

--- screen/components/FormWithUseForm.jsx
+++ screen/components/FormWithUseForm.jsx
@@ -5,19 +5,28 @@ const FormWithUseForm = () => {
 
   const { register, handleSubmit } = useForm();
 
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h2>Introduzca:</h2>
+
       <h3>Nombre y apellidos</h3>
-      <input type='text'></input>
+      <input type='text' {...register('name')}></input>
+
       <h3>Contraseña</h3>
-      <input type='password'></input>
+      <input type='password' {...register('password')}></input>
+
       <h3>Edad</h3>
-      <input type='number'></input>
+      <input type='number' {...register('number')}></input>
+
       <h3>Email</h3>
-      <input type='mail'></input>
-      
+      <input type='mail' {...register('mail')}></input>
+
+      <br></br>
       <input type='submit' value='Enviar' />
     </form>
     </>
