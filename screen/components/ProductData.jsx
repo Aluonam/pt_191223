c2eb18408a3f0a578b1ab41de0e11f1e76547b1b
Paula Cano <paulacanoroman@gmail.com>
2023-12-19 20:55:58 +0100

    feat: api call with defensive programming

--- screen/components/ProductData.jsx
+++ screen/components/ProductData.jsx
@@ -4,11 +4,15 @@ const ProductData = () => {
 
     const [dataAPI, setDataAPI] = useState([])
 
-    const callAPI = ()=>{
+    const callAPI = async ()=>{
         try{
-
+            const url = await fetch(`https://fakestoreapi.com/products?limit=5`);
+            const data = await url.json();
+            console.log(data)
         }catch(error){error, "error detected"}
     }
+
+    callAPI()
   return (
     <div>
         
