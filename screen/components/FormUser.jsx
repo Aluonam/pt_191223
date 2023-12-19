98d811748fb602df17e4b2ac0dc00622d054d0f9
Paula Cano <paulacanoroman@gmail.com>
2023-12-19 21:31:57 +0100

    feat: install useform and skeleton component

---
+++ screen/components/FormUser.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useForm } from 'react-hook-form';
+
+const UseformExample = () => {
+
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <>
+    <h2>Formulario con useForm</h2>
+    <form>
+
+    </form>
+    </>
+  )
+}
+
+export default UseformExample
